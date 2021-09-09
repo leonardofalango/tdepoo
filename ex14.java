c1ee7cc99f0bfb68277bd8654541352f44672363
leonardofalango <81644264+leonardofalango@users.noreply.github.com>
2021-09-09 17:58:15 -0300

    Update ex14.java

--- ex14.java
+++ ex14.java
@@ -1,3 +1,18 @@
 /* 
 #Exercicio 14
+
+14.Crie uma variável do tipo int, atribuindo um valor a ela. Depois crie uma variável do tipo double, atribuindo a ela o valor da primeira variável criada
+
 */
+
+
+public class TdeEx14 {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int x = 10;
+        double y = x;
+
+        System.out.println(y);
+    }
+}
+
+
