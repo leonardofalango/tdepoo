63ef33a3845712fd8f9bd8bde626f1b7a8aeeb23
leonardofalango <81644264+leonardofalango@users.noreply.github.com>
2021-09-13 19:22:41 -0300

    Create ex19.java

---
+++ ex19.java
@@ -0,0 +1,17 @@
+/*
+#Ex 19
+
+19.Calcule o resto da divisão de 99 por 4
+
+/*
+
+//import java.io.IOException;
+
+public class TdeEx19 {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int x = 99, y = 4, resto;
+        resto = x%y;
+
+        System.out.println(resto);
+    }
+}
