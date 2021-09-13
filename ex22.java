f829b1761f1ba50c06e2528b7432c7a534a97552
leonardofalango <81644264+leonardofalango@users.noreply.github.com>
2021-09-13 19:53:20 -0300

    Create ex22.java

---
+++ ex22.java
@@ -0,0 +1,24 @@
+
+
+22.Agora compare se o valor das duas variáveis acima é igual.
+
+
+public class TdeEx22 {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        float numero;
+        int i;
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("Entre com um numero inteiro: ");
+        i = scnr.nextInt();
+        System.out.println("Entre com um numero float: ");
+        numero = scnr.nextFloat();
+
+        if(numero == i){
+            System.out.println("Os numeros sao iguais");
+        }
+        else{
+            System.out.println("Os numeros sao diferentes");
+        }
+    }
+}
