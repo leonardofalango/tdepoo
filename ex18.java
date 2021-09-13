a2bd90e59834e447b3a0fceb0fc22b2476fd3a0e
leonardofalango <81644264+leonardofalango@users.noreply.github.com>
2021-09-13 19:13:28 -0300

    Update ex18.java

--- ex18.java
+++ ex18.java
@@ -10,11 +10,15 @@
 
 public class TdeEx18 {
     public static void main(String[] args) throws IOException, InterruptedException {
-        String frase, conc = "–Exemplo String";
+        String frase, conc = "–Exemplo String", str = "Exemplo string";
         Scanner scnr = new Scanner(System.in);
+        int i = 0;
 
         System.out.println("Entre com os dados: ");
-        frase = scnr.nextLine() + conc; 
+        frase = scnr.nextLine() + str;
+        i = frase.length();
+        frase = frase + conc;
         System.out.printf("A frase escolhida foi:\n %s",frase);
+        System.out.printf("A frase escolhida possui:\n%i Caracteres", i );
     }
 }
