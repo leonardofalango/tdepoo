21611a054f837a3103702a10bdb13c8dd2429f83
leonardofalango <81644264+leonardofalango@users.noreply.github.com>
2021-09-13 15:12:22 -0300

    Update ex15.java

--- ex15.java
+++ ex15.java
@@ -1,10 +1,21 @@
-/* -------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-
+/*
 #Exercicio 15
 
 15.Implemente um programa que receba dados digitados pelo usuário e atribua a uma variável do tipo String e imprima a frase inserida no console.
 
+*/
 
-R: 
+//import java.util.Scanner
 
-/* -------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+public class TdeEx15 {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String frase= "";
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("Entre com os dados: ");
+        frase = scnr.nextLine();
+        System.out.printf("A frase escolhida foi: %s",frase);
+    }
+}
+
+
