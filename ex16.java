45ee17230d7fdb84cd6647a95ca349995276b3ac
leonardofalango <81644264+leonardofalango@users.noreply.github.com>
2021-09-13 15:30:39 -0300

    Update ex16.java

--- ex16.java
+++ ex16.java
@@ -8,11 +8,11 @@
 
 public class TdeEx16 {
     public static void main(String[] args) throws IOException, InterruptedException {
-        String frase, conc = "Exemplo String";
+        String frase, conc = " Exemplo String";
         Scanner scnr = new Scanner(System.in);
 
         System.out.println("Entre com os dados: ");
-        frase = scnr.nextLine() + conc; //Para colocar espaco entre "Exemplo String" e a frase de entrada do usr, basta colocar "+' '+" entre o scnr e a variavel conc.
+        frase = scnr.nextLine() + conc; 
         System.out.printf("A frase escolhida foi:\n %s",frase);
     }
 }
