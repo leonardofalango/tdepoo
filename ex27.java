706c47f7c56cf9fbf8fad3db38b62ec9e6420ca5
leonardofalango <81644264+leonardofalango@users.noreply.github.com>
2021-09-14 10:33:12 -0300

    Update ex27.java

--- ex27.java
+++ ex27.java
@@ -5,3 +5,27 @@
 
 /*
 
+public class TdeEx27 {
+    public static void main(String[] args) {
+        int contador=0, numero = 0, i;
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("Entre com um valor: ");
+        i = scnr.nextInt();
+
+        for (int u = 1; u <= i; u++) {
+            if (i % u == 0) {
+                contador++;
+                numero = u;
+            }
+        }
+
+        if (contador == 2) {
+            System.out.println("O numero "+numero+" eh primo");
+        }
+
+        if (i%2==0){
+            System.out.println("O numero eh par");
+        }
+    }
+}
